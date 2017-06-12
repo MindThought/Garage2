0a7aee3ac26b93e596785826cbaaee9dc19f6701
Unknown <mindthought37@gmail.com>
2017-06-12 10:06:58 +0200

    ParkedVehicle
    Basic ParkedVehicle added

---
+++ Garage2/Models/ParkedVehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Models
+{
+    public enum Types { Car, Bike, Plane, Boat, Truck };
+
+    public class ParkedVehicle
+    {
+        public Types Type { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int NumberOfWheels { get; set; }
+        public DateTime TimeParked { get; set; }
+    }
+}
